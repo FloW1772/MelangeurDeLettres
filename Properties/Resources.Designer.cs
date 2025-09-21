70e8c4156e9f863355f1cda7b560f02914ded27c
FloW1772 <58610562+FloW1772@users.noreply.github.com>
2025-09-21 03:22:04 +0200

    update et fin de mise en forme systeme

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -33,7 +33,7 @@ namespace MelangeurDeLettres.Properties
             {
                 if (object.ReferenceEquals(resourceMan, null))
                 {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("MelangeurDeLettres.Properties.Resources", typeof(Resources).GetTypeInfo().Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("MelangeurDeLettres.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
